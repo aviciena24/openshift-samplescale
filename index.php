62b77d85d33880eda924db93f04954a058425058
aviciena24 <105403806+aviciena24@users.noreply.github.com>
2022-05-12 13:46:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html>
 <body>
 
-<h2>HTML Image</h2>
+<h2><?php
+  print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+?></h2>
 <img src="ghofur.jpeg" alt="Trulli" width="500" height="333">
 
 </body>
